5ba8b2d2db1131b5499b62409f5d7d356de673b1
Matt Seltz <matiselt@outlook.es>
2022-10-11 17:18:02 -0300

    Constantes declaradas

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form");
+const titulo = document.getElementById("titulo");
+const tarea = document.getElementById("tarea");
+const crear = document.getElementById("crear");
+const gridCont = document.querySelector("grid-cont");
+
+form.addEventListener("submit",e => {
+    e.preventDefault();
+});
